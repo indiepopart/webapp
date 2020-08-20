253e78d6d61496031709c28e130fc1e62adad20e
indiepopart <jimena@gmail.com>
2020-08-20 00:35:55 -0300

    add webapp to docker compose

--- src/test/java/com/okta/developer/webapp/WebApplicationTests.java
+++ src/test/java/com/okta/developer/webapp/WebApplicationTests.java
@@ -2,8 +2,10 @@ package com.okta.developer.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebApplicationTests {
 
 	@Test
