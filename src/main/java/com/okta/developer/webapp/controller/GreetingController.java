a8edb75615839c9e4646d450f77698e56f5bebb1
indiepopart <jimena@gmail.com>
2020-08-31 04:22:41 -0300

    add print hostname

--- src/main/java/com/okta/developer/webapp/controller/GreetingController.java
+++ src/main/java/com/okta/developer/webapp/controller/GreetingController.java
@@ -1,17 +1,32 @@
 package com.okta.developer.webapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.InetAddress;
+
 @Controller
 public class GreetingController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
 
     @GetMapping(value = "/greeting")
     @ResponseBody
     public String getGreeting(@AuthenticationPrincipal OidcUser oidcUser) {
-        return "Hello " + oidcUser.getFullName();
+        String serverUsed = "unknown";
+        try {
+            InetAddress host = InetAddress.getLocalHost();
+            serverUsed = host.getHostName();
+        } catch (Exception e){
+            logger.error("Could not get hostname", e);
+        }
+        logger.info("Request responded by " + serverUsed);
+        return "Hello " + oidcUser.getFullName() + ", you are connected to " + serverUsed;
     }
 }
